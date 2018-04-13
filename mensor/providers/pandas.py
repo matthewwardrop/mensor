1fd33a7804a3531228f0e283999d0ae84a61c5a8
Matthew Wardrop <mawardrop@netflix.com>
2018-04-13 15:59:47 -0700

    Add first unittest using tox/pytest/unittest.

---
+++ mensor/providers/pandas.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import six
+
+from mensor.measures.context import Constraint
+from mensor.measures.provider import MeasureProvider
+
+
+class PandasMeasureProvider(MeasureProvider):
+
+    def __init__(self, name, data=None, **kwargs):
+        MeasureProvider.__init__(self, name, **kwargs)
+        if isinstance(data, str):
+            data = pd.read_csv(data)
+        self.data = data
+
+    def _evaluate(self, unit_type, measures, where=None, segment_by=None, **opts):
+        """
+        Should return a dataframe satisfying the following properties:
+        - measures should, depending on distribution, provide columns:
+            <measure>:norm:sum
+            <measure>:norm:sos
+            <measure>:norm:count
+        - dimensions should correspond to a single column, with the value being
+            the value of that dimension.
+        """
+
+        def measure_map(name, op):
+            return lambda df: op(df[name])
+
+        def measure_maps(measures):
+            d = {}
+            for measure in measures:
+                if measure.external:
+                    continue
+                d[measure.name + ':norm:sum'] = measure_map(measure.name, lambda x: x)
+                d[measure.name + ':norm:sos'] = measure_map(measure.name, lambda x: x**2)
+                d[measure.name + ':norm:count'] = measure_map(measure.name, lambda x: 1 * x.notnull())
+            return d
+
+        measure_cols = measure_maps(measures)
+
+        d = (
+            self.data
+            .rename(
+                columns={identifier.expr: identifier.name for identifier in self.identifiers},
+            )
+            .rename(
+                columns={dimension.expr: dimension.name for dimension in self.dimensions},
+            )
+            .rename(
+                columns={dimension.expr: dimension.name for dimension in self.measures},
+            )
+            .assign(**measure_cols)
+        )
+
+        for constraint in where:
+            assert isinstance(constraint, Constraint), 'Unexpected constraint type: {}'.format(type(constraint))
+            d = d.query('{} == {}'.format(
+                constraint.expr,
+                constraint.rhs if not isinstance(constraint.rhs, six.string_types) else '"{}"'.format(constraint.rhs)
+            ))
+
+        if len(segment_by) > 0:
+            d = (
+                d
+                .groupby([x.via_name for x in segment_by if not x.external])
+                .sum()  # TODO: potentially expensive
+                [list(measure_cols)]
+                .reset_index()
+            )
+        else:
+            d = d[list(measure_cols)].sum()
+
+        return d
+
+    def _is_compatible_with(self, other):
+        return False
---
+++ tests/test_pandas_provider.py
@@ -0,0 +1,60 @@
+import os
+import unittest
+
+from mensor.measures.registry import MeasureRegistry
+from mensor.providers.pandas import PandasMeasureProvider
+
+
+class PandasMeasureProviderTests(unittest.TestCase):
+
+    def setUp(self):
+        self.registry = MeasureRegistry()
+
+        data_dir = os.path.join(os.path.dirname(__file__), 'data')
+
+        people = (
+            PandasMeasureProvider(
+                name='people',
+                data=os.path.join(data_dir, 'people.csv')
+            )
+            .add_identifier('person', expr='id', role='primary')
+            .add_identifier('geography', expr='id_country', role='foreign')
+            .add_dimension('name')
+            .add_measure('age')
+        )
+        self.registry.register(people)
+
+        transactions = (
+            PandasMeasureProvider(
+                name='transactions',
+                data=os.path.join(data_dir, 'transactions.csv')
+            )
+            .add_identifier('transaction', expr='id', role='primary')
+            .add_identifier('person:buyer', expr='id_buyer', role='foreign')
+            .add_identifier('person:seller', expr='id_seller', role='foreign')
+            .add_measure('value')
+        )
+        self.registry.register(transactions)
+
+        self.registry.show()
+
+    def test_simple(self):
+        strategy = self.registry.evaluate(
+            'transaction',
+            measures=['person:seller/age'],
+            segment_by=['person:buyer/name'],
+            where=None
+        )
+
+        df = strategy.run()
+
+        self.assertGreater(len(df), 0)
+        self.assertTrue(
+            set(df.columns) == set([
+                'person:seller/age:norm:count',
+                'person:seller/age:norm:sos',
+                'person:seller/age:norm:sum',
+                'person:buyer/name'
+            ])
+        )
+        self.assertFalse(df.duplicated('person:buyer/name').any())
