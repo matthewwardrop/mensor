306c4296dbefbb322cf09e336782d988f174d29e
Matthew Wardrop <mawardrop@netflix.com>
2018-04-19 16:08:53 -0700

    Initial SQL dialect support.

--- mensor/providers/sql.py
+++ mensor/providers/sql.py
@@ -3,6 +3,8 @@ import jinja2
 from mensor.measures.provider import MeasureProvider
 from mensor.measures.types import AGG_METHODS
 
+# TODO: Consider using sqlalchemy to generate SQL
+
 TEMPLATE = jinja2.Template("""
 WITH
     "base_query" AS (
@@ -29,10 +31,14 @@ JOIN "{{join.name}}" ON
 {%- endfor -%}
 {%- endfor %}
 {%- endif %}
-{%- if dimensions|length > 0 %}
+{%- if groupby|length > 0 %}
 GROUP BY
-{%- for dimension in dimensions %}
-{%- if loop.index > 1 %},{% endif %} {{ loop.index }}
+{%- for gb in groupby %}
+{%- if positional_groupby %}
+{%- if loop.index > 1 %},{% endif %} {{ gb }}
+{%- else %}
+    {% if loop.index > 1 %},{% endif %} {{gb}}
+{%- endif %}
 {%- endfor %}
 {%- endif %}
 """.strip())
@@ -52,15 +58,42 @@ FROM {{table}}
 """.strip())
 
 
+# Dialects
+class SQLDialect(object):
+
+    QUOTE_COL = '"'
+    QUOTE_STR = "'"
+
+    POSITIONAL_GROUPBY = True
+
+    MEASURE_AGG_METHODS = {
+        AGG_METHODS.SUM: lambda x: "SUM({})".format(x),
+        AGG_METHODS.MEAN: lambda x: "AVG({})".format(x),
+        AGG_METHODS.SQUARE_SUM: lambda x: "SUM(POW({}, 2)".format(x),
+        AGG_METHODS.COUNT: lambda x: "COUNT({})".format(x)
+    }
+
+
+class PrestoDialect(SQLDialect):
+
+    POSITIONAL_GROUPBY = True
+
+
+DIALECTS = {
+    'presto': PrestoDialect
+}
+
+
 class SQLMeasureProvider(MeasureProvider):
     # TODO: Handle unit-aggregation
 
-    def __init__(self, *args, sql=None, db_client=None, **kwargs):
+    def __init__(self, *args, sql=None, db_client=None, dialect='presto', **kwargs):
         assert db_client is not None, "Must specify an (Omniduct-compatible) database client."
 
         MeasureProvider.__init__(self, *args, **kwargs)
         self._sql = sql
         self.db_client = db_client
+        self.dialect = DIALECTS[dialect]
 
         self.add_measure('count', distribution=None)
 
@@ -97,7 +130,6 @@ class SQLMeasureProvider(MeasureProvider):
         return aggs
 
     def _get_dimensions_sql(self, dimensions, joins):
-
         dims = []
 
         for dimension in dimensions:
@@ -111,19 +143,48 @@ class SQLMeasureProvider(MeasureProvider):
 
         return dims
 
+    def _get_groupby_sql(self, dimensions, joins):
+
+        dims = []
+
+        count = 1
+        for dimension in dimensions:
+            if not dimension.external and not dimension.private:
+                if self.dialect.POSITIONAL_GROUPBY:
+                    dims.append(count)
+                    count += 1
+                else:
+                    dims.append('"base_query"."{m}"'.format(m=dimension.expr))
+
+        for j in joins:
+            for dimension in j.dimensions:
+                if not dimension.private and dimension not in j.right_on:
+                    if self.dialect.POSITIONAL_GROUPBY:
+                        dims.append(count)
+                        count += 1
+                    else:
+                        dims.append('"{n}"."{m}"'.format(n=j.name, m=dimension.via_name))
+
+        return dims
+
     def _get_ir(self, unit_type, measures=None, segment_by=None, where=None, joins=None, via=None, **opts):
         sql = TEMPLATE.render(
             base_sql=self.sql,
             provider=self,
             dimensions=self._get_dimensions_sql(segment_by, joins),
             measures=self._get_measures_sql(measures, joins),
+            groupby=self._get_groupby_sql(segment_by, joins),
             joins=joins,
-            filter=' AND '.join(where) if where else ''
+            filter=' AND '.join(where) if where else '',
+            positional_groupby=self.dialect.POSITIONAL_GROUPBY
         )
         return sql
 
     def get_sql(self, *args, **kwargs):
-        return self.get_ir(*args, **kwargs)
+        sql = self.get_ir(*args, **kwargs)
+        if self.dialect.QUOTE_COL != '"':
+            sql.replace('"', self.dialect.QUOTE_COL)
+        return sql
 
     def _evaluate(self, unit_type, measures=None, segment_by=None, where=None, joins=None, **opts):
         return self.db_client.query(self.get_sql(
@@ -137,12 +198,7 @@ class SQLMeasureProvider(MeasureProvider):
 
     @property
     def _measure_agg_methods(self):
-        return {
-            AGG_METHODS.SUM: lambda x: "SUM({})".format(x),
-            AGG_METHODS.MEAN: lambda x: "AVG({})".format(x),
-            AGG_METHODS.SQUARE_SUM: lambda x: "SUM(POW({}, 2)".format(x),
-            AGG_METHODS.COUNT: lambda x: "COUNT({})".format(x)
-        }
+        return self.dialect.MEASURE_AGG_METHODS
 
 
 class SQLTableMeasureProvider(SQLMeasureProvider):
