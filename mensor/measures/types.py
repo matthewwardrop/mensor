d974b4093723941d7ccf1ed06e44eb5a23f2c70e
Matthew Wardrop <mawardrop@netflix.com>
2018-07-17 16:54:46 -0700

    Change structure of FeatureSpec object in order to future-proof it.

--- mensor/measures/types.py
+++ mensor/measures/types.py
@@ -124,16 +124,16 @@ class FeatureSpec:
 
     ALLOWED_TRANSFORMS = ['pre_agg', 'agg', 'post_agg', 'pre_rebase_agg', 'rebase_agg', 'post_rebase_agg', 'alias']
 
-    def __init__(self, source, alias=None, transforms=None):
-        self.source = source
-        self.alias = alias or None
-        self.transforms = transforms or {}
+    def __init__(self, alias, source=None, transforms=None):
+        self._alias = alias
+        self._source = source or alias
+        self._transforms = transforms or {}
 
     def __repr__(self):
         return "<Measure" + (f"[{self.alias}]" if self.alias else "") + (" (with transforms)" if self.transforms else "") + ">"
 
-    def with_alias(self, alias):
-        self.alias = alias
+    def source(self, source):
+        self._source = source
         return self
 
     def with_transforms(self, unit_type, pre_agg=None, agg=None, post_agg=None, pre_rebase_agg=None, rebase_agg=None, post_rebase_agg=None, alias=None):
@@ -144,33 +144,32 @@ class FeatureSpec:
             if variables[name] is not None
         }
         if transforms:
-            self.transforms[unit_type] = transforms
+            self._transforms[unit_type] = transforms
         return self
 
     def get_attrs(self, unit_type):
         attrs = {
-            'transforms': self.transforms
+            'transforms': self._transforms
         }
-        if self.alias:
-            if unit_type not in attrs['transforms']:
-                attrs['transforms'][unit_type] = {}
-            attrs['transforms'][unit_type]['alias'] = self.alias
+        if unit_type not in attrs['transforms']:
+            attrs['transforms'][unit_type] = {}
+        attrs['transforms'][unit_type]['alias'] = self._alias
         return attrs
 
     def as_source_with_attrs(self, unit_type):
-        return self.source, self.get_attrs(unit_type)
+        return self._source, self.get_attrs(unit_type)
 
     @property
     def as_dict(self):
         return {
-            'source': self.source,
-            'alias': self.alias,
-            'transforms': self.transforms
+            'source': self._source,
+            'alias': self._alias,
+            'transforms': self._transforms
         }
 
     @property
     def as_yaml(self):
-        return yaml.dump(self.as_feature, default_flow_style=False, indent=4)
+        return yaml.dump(self.as_dict, default_flow_style=False, indent=4)
 
 
 class MeasureSpec(FeatureSpec):
